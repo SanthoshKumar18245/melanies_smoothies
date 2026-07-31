27335738ad77d753165e168e056e69c9b0758f52
SanthoshKumar18245 <133011171+SanthoshKumar18245@users.noreply.github.com>
2026-07-31 15:02:23 +0530

    Update streamlit_app.py
    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -36,5 +34,9 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered '+ name_on_order +'!', icon="✅")
+
+
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
 
     
